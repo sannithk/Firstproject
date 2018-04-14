5d43e7cd333e81e0f7e034b35e901c06620454b0
Sannith Kumar <Sannith Kumar@192.168.0.36>
2018-04-13 20:52:19 -0500

    First project is commited

---
+++ FirstProject/src/IfElse.java
@@ -0,0 +1,25 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		
+		a=12;
+		b=5;
+		c=22;
+		
+		if(a>b && a>c)
+		{
+			System.out.println(a);
+		}
+		else if(b>c) {
+			System.out.println(b);
+		}
+		else {
+			System.out.println("C");
+		}
+
+	}
+
+}
---
+++ FirstProject/src/Variables.java
@@ -0,0 +1,27 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int myInt= 12;
+		short myShort= 234;
+		long myLong= 23411;
+		
+		double myDouble = 2.987;
+		float myFloat= 8.1f;
+		
+		char myChar='a';
+		boolean myBoolean=true;
+		byte myByte=123;
+		
+		
+		System.out.println(myInt);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
